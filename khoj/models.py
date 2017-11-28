f2f024c6268afd47536301eba0169d3bace19bc9
Jony Karki <jonykarki@hotmail.com>
2017-11-28 20:53:13 +0545

    database changes

--- khoj/models.py
+++ khoj/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    bimg = models.CharField(max_length=200)
+    bimg = models.URLField()
     btype = models.CharField(max_length=200)
     bname = models.CharField(max_length=200)
     bservices = models.CharField(max_length=400)

6ebac2a77519f7605bb98f576a6507a54b629f7d
Jony Karki <jony.karki@vimarsh.local>
2017-11-29 12:12:43 +0545

    database changes

--- khoj/database.py
+++ khoj/database.py
@@ -17,7 +17,7 @@ class InsertDatabase():
         self.bwebsite = bwebsite
 
         #connect to the database
-        self.conn = db.connect('dkc2gr094item')
+        self.conn = db.connect('dkc2gr094item=')
 
         # get the cursor
         self.c = self.conn.cursor()
